50fbf06dc2cc1525dfe51923b5d229afccc9b611
Faisal Hatila <hatilas1994@gmail.com>
2020-07-13 01:18:01 +0500

    add restaurant form integerated in add restaurant container

--- src/containers/addrestaurant/index.js
+++ src/containers/addrestaurant/index.js
@@ -1,7 +1,17 @@
 import React, { Component } from "react";
+import { AddRestaurantForm } from "../../components";
 
 export default class AddRestaurant extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="container">
+        <div className="row">
+          {/* <LeftMenu /> */}
+          <div className="col-12 mt-4">
+            <AddRestaurantForm />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
