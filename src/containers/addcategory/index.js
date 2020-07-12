e170d2b0596adf0e36ad0de74af4e681bfca8bcc
Faisal Hatila <hatilas1994@gmail.com>
2020-07-13 03:59:39 +0500

    add category form integerated with add category container

--- src/containers/addcategory/index.js
+++ src/containers/addcategory/index.js
@@ -1,7 +1,16 @@
 import React, { Component } from "react";
+import { AddCategoryForm } from "../../components";
 
 export default class AddCategory extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-lg-9 col-md-9 mt-4">
+            <AddCategoryForm />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
