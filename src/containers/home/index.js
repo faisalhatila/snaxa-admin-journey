0d1127b846feaa91c4b05e17475302e50ed99631
Faisal Hatila <hatilas1994@gmail.com>
2020-07-12 22:39:47 +0500

    left menu hidden from dashboard

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -14,8 +14,8 @@ class HomeContainer extends Component {
     return (
       <div className="container">
         <div className="row">
-          <LeftMenu />
-          <div className="col-12 col-lg-9 col-md-9 mt-4">
+          {/* <LeftMenu /> */}
+          <div className="col-12 mt-4">
             <DataCounter />
             <div className="">
               <Chart />
