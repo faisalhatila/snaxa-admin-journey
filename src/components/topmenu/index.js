db36947db03a5b7fd17170356c0e79a7fb935f3f
Faisal Hatila <hatilas1994@gmail.com>
2020-07-16 05:05:34 +0500

    logout button added

--- src/components/topmenu/index.js
+++ src/components/topmenu/index.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TopMenu = (props) => {
+  const [isOpenLogoutBtn, setIsOpenLogoutBtn] = useState(false);
+  const handleDisplayLogoutBtn = () => {
+    if (!isOpenLogoutBtn) {
+      setIsOpenLogoutBtn(true);
+    } else {
+      setIsOpenLogoutBtn(false);
+    }
+  };
   return (
     <div className="topMenuMainDiv d-none d-md-block">
       <div className="container">
@@ -9,15 +17,39 @@ const TopMenu = (props) => {
             <i class="far fa-question-circle"></i>Help
           </p>
           <p>Dashboard</p>
-          <div className="userInfoDiv d-flex align-items-center">
-            <div className="mr-2">
-              <i class="far fa-user"></i>
+          <div>
+            <div className="userInfoDiv d-flex align-items-center">
+              <div className="mr-2">
+                <i class="far fa-user"></i>
+              </div>
+              <div>
+                <p className="userName">Alaska Restaurant</p>
+                <p className="userJoiningStartDate">User Since 2019</p>
+              </div>
+              {!isOpenLogoutBtn ? (
+                <i
+                  class="fas fa-caret-down ml-2"
+                  onClick={handleDisplayLogoutBtn}
+                ></i>
+              ) : null}
+              {isOpenLogoutBtn ? (
+                <i
+                  class="fas fa-sort-up ml-2"
+                  onClick={handleDisplayLogoutBtn}
+                ></i>
+              ) : null}
             </div>
-            <div>
-              <p className="userName">Alaska Restaurant</p>
-              <p className="userJoiningStartDate">User Since 2019</p>
-            </div>
-            <i class="fas fa-caret-down ml-2"></i>
+            <hr className="noMargin" />
+            {isOpenLogoutBtn ? (
+              <div className="logoutDiv d-flex align-items-center position-absolute">
+                <div className="mr-2">
+                  <i class="fas fa-power-off"></i>
+                </div>
+                <div>
+                  <p className="noMargin userName">Logout</p>
+                </div>
+              </div>
+            ) : null}
           </div>
         </div>
       </div>
