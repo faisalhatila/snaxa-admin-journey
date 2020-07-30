d587f1d4719bed6cb1d4a27f5ce8dc98d7e03807
faisalhatila <hatilas1994@gmail.com>
2020-07-30 07:46:56 -0700

    faq details integerated

--- src/containers/faqs/index.js
+++ src/containers/faqs/index.js
@@ -1,7 +1,15 @@
-import React, { useState } from "react";
-
+import React from "react";
+import { Faqdetails } from "../../components";
+// let AddOrderStatus;
 const Faqs = (props) => {
-  return <div></div>;
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12 mt-4">
+          <Faqdetails />
+        </div>
+      </div>
+    </div>
+  );
 };
-
 export default Faqs;
