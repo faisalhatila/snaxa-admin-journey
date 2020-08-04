4178ae622cdbd9bc3434edab2a4c8f24ebdb2cd0
faisalhatila <hatilas1994@gmail.com>
2020-08-04 04:15:03 -0700

    sitemap container created

---
+++ src/containers/sitemap/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Faqdetails } from "../../components";
+// let AddOrderStatus;
+const Sitemap = (props) => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12 mt-4">
+          <Faqdetails />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Sitemap;
