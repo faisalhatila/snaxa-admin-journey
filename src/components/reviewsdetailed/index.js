454d0db445b22991a840cc81f7189231d697013b
Faisal Hatila <hatilas1994@gmail.com>
2020-08-07 01:58:46 +0500

    reviews detailed component created

---
+++ src/components/reviewsdetailed/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { ReviewTableDetailRating, ReviewTableDetail } from "..";
+
+const ReviewsDetailed = () => {
+  return (
+    <div>
+      <ReviewTableDetailRating />
+      <ReviewTableDetail />
+    </div>
+  );
+};
+export default ReviewsDetailed;
