01195852b7a00d753d733bd8ae613fa8134c8401
faisalhatila <hatilas1994@gmail.com>
2020-08-09 05:08:46 -0700

    reviews container created

---
+++ src/containers/reviews/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { ReviewTableShort } from "../../components";
+// let OrderDetails;
+const Reviews = (props) => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12 mt-4">
+          <ReviewTableShort />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Reviews;
