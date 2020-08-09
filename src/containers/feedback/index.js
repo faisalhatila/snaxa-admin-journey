f4b95810a5e818d820c63ec5a754d986b9bd7046
faisalhatila <hatilas1994@gmail.com>
2020-08-09 07:18:58 -0700

    feedback table integerated

--- src/containers/feedback/index.js
+++ src/containers/feedback/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { ReviewsTable } from "../../components";
+import { FeedbackTable } from "../../components";
 // let OrderDetails;
 const Feedback = (props) => {
   return (
     <div className="container">
       <div className="row">
         <div className="col-12 mt-4">
-          <ReviewsTable />
+          <FeedbackTable />
         </div>
       </div>
     </div>
