c732e81387cdea06947b266dbeba1b2a7b01541e
faisalhatila <hatilas1994@gmail.com>
2020-08-09 04:38:50 -0700

    privacy container created and privacy form integerated

---
+++ src/containers/privacy/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Privacyform } from "../../components";
+// let AddOrderStatus;
+const Privacy = (props) => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12 mt-4">
+          <Privacyform />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Privacy;
