31e18759bd2fdd971b5b293ad85f366e288b9820
faisalhatila <hatilas1994@gmail.com>
2020-08-09 04:50:40 -0700

    contact queries container created

---
+++ src/containers/contactqueries/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { ContactQureyTable } from "../../components";
+// let OrderDetails;
+const ContactQueries = (props) => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12 mt-4">
+          <ContactQureyTable />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default ContactQueries;
