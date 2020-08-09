9e961708c6c7c56538331cc7b276723d6fa14b20
faisalhatila <hatilas1994@gmail.com>
2020-08-09 07:10:14 -0700

    feedback container created

---
+++ src/containers/feedback/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { ReviewsTable } from "../../components";
+// let OrderDetails;
+const Feedback = (props) => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12 mt-4">
+          <ReviewsTable />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Feedback;
