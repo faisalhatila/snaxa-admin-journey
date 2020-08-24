4a91f0fcd6d84691b6b1e7f45f19bdbd83d4acc9
faisalhatila <hatilas1994@gmail.com>
2020-08-23 22:52:41 -0700

    add category form, comment and grabage input props removed

--- src/components/addcategoryform/index.js
+++ src/components/addcategoryform/index.js
@@ -1,25 +1,16 @@
-// import { ImageUpload } from "..";
 import React, { useState, useEffect } from "react";
 import { useAuth } from "./../../shared/hooks/auth-hooks";
 import { useHttpClient } from "./../../shared/hooks/http-hook";
 import Select from "react-select";
 import makeAnimated from "react-select/animated";
 const animatedComponents = makeAnimated();
-// import { Select } from "react-dropdown-select";
-
-// let AddCategoryForm;
 const AddCategoryForm = (props) => {
   const [categoryName, setCategoryName] = useState("");
   const [restaurant, setRestaurant] = useState("");
-  // const [itemDescription, setItemDescription] = useState("");
   const [itemPriority, setItemPriority] = useState("");
   const [itemStatus, setItemStatus] = useState(true);
-  // const [categoryNameError, setIategoryNameError] = useState("");
-  // const [itemDescriptionError, setItemDescriptionError] = useState("");
-  // const [itemPriorityError, setItemPriorityError] = useState("");
   const [categories, setCategories] = useState([]);
   const [categoryId, setCategoryId] = useState([]);
-
   const { userId, token } = useAuth();
   const { isLoading, error, sendRequest, clearError } = useHttpClient();
   const [data, setData] = useState([]);
@@ -29,9 +20,7 @@ const AddCategoryForm = (props) => {
 
   useEffect(() => {
     const dashboard = async () => {
-      // console.log(event);
       setEditing(false);
-      // setRestaurant(event.index);
       setRestaurantID(props.restaurantId);
       try {
         const responseData = await sendRequest(
@@ -80,89 +69,23 @@ const AddCategoryForm = (props) => {
           return { index, value: i.restaurant, label: i.name };
         });
         setColourOptions(temp);
-      } catch (err) {
-        // console.log("err", err);
-      }
+      } catch (err) {}
     };
     if (token && userId) dashboard();
   }, [token, userId, sendRequest]);
 
   const handleChangecategoryName = (event) => {
     setCategoryName(event.target.value);
-    // categoryNameError: "",
   };
-  // const handleChangeItemDescription = (event) => {
-  // 	// setState({
-  // 	// itemDescription: event.target.value,
-  // 	// itemDescriptionError: "",
-  // 	// });
-  // };
   const handleChangeItemPriority = (event) => {
     setItemPriority(event.target.value);
-    // itemPriorityError: "",
   };
   const handleChangeItemStatus = (event) => {
     setItemStatus(event.target.checked);
   };
   const validate = () => {
-    // const { categoryName, itemDescription, itemPriority } = state;
-    // let {
-    //   categoryNameError,
-    //   itemDescriptionError,
-    //   itemPriorityError,
-    // } = state;
-    // if (!categoryName) {
-    //   categoryNameError = "Please Enter Category Name";
-    // } else {
-    //   categoryNameError = "";
-    // }
-    // if (!itemDescription) {
-    //   itemDescriptionError = "Please Enter Item Name";
-    // } else {
-    //   itemDescriptionError = "";
-    // }
-    // if (!itemPriority) {
-    //   itemPriorityError = "Please Enter Item Name";
-    // } else {
-    //   itemPriorityError = "";
-    // }
-    // if (categoryNameError || itemPriorityError || itemDescriptionError) {
-    //   setState({
-    //     categoryNameError,
-    //     itemPriorityError,
-    //     itemDescriptionError,
-    //   });
-    //   return false;
-    // }
     return true;
   };
-  // const handleSelectRestaurant = async (event) => {
-  //   console.log(event);
-  //   setEditing(false);
-  //   setRestaurant(event.index);
-  //   setRestaurantID(event.value);
-  //   try {
-  //     const responseData = await sendRequest(
-  //       `${process.env.REACT_APP_BACKEND_URL}/get-categories`,
-  //       "POST",
-  //       {
-  //         "Content-Type": "application/json",
-  //         Authorization: "Bearer " + token,
-  //       },
-  //       JSON.stringify({
-  //         userId,
-  //         restaurantId: event.value,
-  //       })
-  //     );
-  //     console.log("responseData", responseData);
-  //     setCategories(responseData.existingCategories);
-  //     setCategoryName("");
-  //     setItemPriority("");
-  //     setItemStatus(true);
-  //   } catch (err) {
-  //     // console.log("err", err);
-  //   }
-  // };
   const handleSubmit = async (event) => {
     event.preventDefault();
     const isValid = validate();
@@ -181,7 +104,6 @@ const AddCategoryForm = (props) => {
             userId,
             categoryName: categoryName,
             priority: itemPriority,
-            // restaurantId: restaurant,
             restaurantId: props.restaurantId,
             status: itemStatus,
             categoryId,
@@ -190,13 +112,10 @@ const AddCategoryForm = (props) => {
         console.log("responseData", responseData);
         setCategories(responseData.existingCategories);
         setCategoryName("");
-        // setRestaurant(1);
         setItemPriority("");
         setItemStatus(true);
         setEditing(false);
-      } catch (err) {
-        // console.log("err", err);
-      }
+      } catch (err) {}
     }
   };
   const handleDeleteCategory = async (id) => {
@@ -218,12 +137,9 @@ const AddCategoryForm = (props) => {
       console.log("responseData", responseData);
       setCategories(responseData.existingCategories);
       setCategoryName("");
-      // setRestaurant(1);
       setItemPriority("");
       setItemStatus(true);
-    } catch (err) {
-      // console.log("err", err);
-    }
+    } catch (err) {}
   };
   const handleViewEditCategory = async (id) => {
     setEditing(true);
@@ -241,19 +157,15 @@ const AddCategoryForm = (props) => {
         })
       );
       console.log("responseData", responseData);
-      // setCategories(responseData.existingCategories);
       setCategoryName(responseData.existingCategories.categoryName);
       setItemPriority(responseData.existingCategories.priority);
       setItemStatus(responseData.existingCategories.status);
       setCategoryId(responseData.existingCategories._id);
-    } catch (err) {
-      // console.log("err", err);
-    }
+    } catch (err) {}
   };
 
   const handleCancelEdit = () => {
     setCategoryName("");
-    // setRestaurant(1);
     setItemPriority("");
     setItemStatus(true);
     setEditing(false);
@@ -263,9 +175,6 @@ const AddCategoryForm = (props) => {
   if (!isLoading && data.length > 0)
     content = (
       <div className="row">
-        {/* <div className="col-4 col-lg-3 col-md-3 updateVendorFormTitle">
-          Category Details
-        </div> */}
         <div className="col-12 customerDetailFormMainDiv d-lg-flex d-md-flex">
           <form className="col-12 col-md-6 col-lg-6 updateVendorForm">
             <div className="row">
@@ -274,54 +183,21 @@ const AddCategoryForm = (props) => {
                 <input
                   type="text"
                   class="form-control"
-                  id="exampleInputEmail1"
-                  aria-describedby="emailHelp"
                   placeholder="Enter Category Name"
                   onChange={handleChangecategoryName}
                   value={categoryName}
                 />
-                {/* {categoryNameError ? (
-									<div
-										style={{
-											textAlign: "center",
-											color: "red",
-											fontWeight: "bold",
-										}}>
-										{categoryNameError}
-									</div>
-								) : null} */}
               </div>
-              {/* <div class="form-group col-12 col-md-6 col-lg-6">
-                <label for="exampleFormControlSelect1">Select Restaurant</label>
-                <Select
-                  defaultValue={colourOptions[restaurant]}
-                  options={colourOptions}
-                  formatGroupLabel={formatGroupLabel}
-                  onChange={handleSelectRestaurant}
-                />
-              </div> */}
             </div>
             <div class="form-group">
               <label for="exampleInputEmail1">Priority</label>
               <input
                 type="text"
                 class="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
                 placeholder="Priority"
                 onChange={handleChangeItemPriority}
                 value={itemPriority}
               />
-              {/* {itemPriorityError ? (
-								<div
-									style={{
-										textAlign: "center",
-										color: "red",
-										fontWeight: "bold",
-									}}>
-									{itemPriorityError}
-								</div>
-							) : null} */}
             </div>
             <div class="custom-control custom-switch">
               <input
@@ -354,7 +230,6 @@ const AddCategoryForm = (props) => {
             )}
           </form>
           <div className="col-12 col-md-6 col-lg-6 updateVendorForm">
-            {" "}
             <table class="table table-hover">
               <thead style={{ backgroundColor: "gray", color: "#fff" }}>
                 <tr>
@@ -389,12 +264,9 @@ const AddCategoryForm = (props) => {
                     </tr>
                   );
                 })}
-
-                {/* );
-                })} */}
               </tbody>
             </table>
-          </div>{" "}
+          </div>
         </div>
       </div>
     );
