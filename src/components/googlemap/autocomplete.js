67b7b5db3108b501c2905b48755d0214be4abca0
faisalhatila <hatilas1994@gmail.com>
2020-08-24 05:10:22 -0700

    auto complete component file created

---
+++ src/components/googlemap/autocomplete.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from "react-places-autocomplete";
+const AutoComplete = () => {
+  return <div></div>;
+};
+
+export default AutoComplete;
