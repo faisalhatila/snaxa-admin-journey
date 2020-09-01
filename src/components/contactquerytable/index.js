3dfc370aa0f15c91279225cdbe667af411687ee1
faisalhatila <hatilas1994@gmail.com>
2020-09-01 14:40:39 +0500

    contact table api integerated

--- src/components/contactquerytable/index.js
+++ src/components/contactquerytable/index.js
@@ -16,10 +16,9 @@ const ContactQureyTable = (props) => {
   // const auth = useContext(AuthContext);
   useEffect(() => {
     const dashboard = async () => {
-      console.log("new-restaurants");
       try {
         const responseData = await sendRequest(
-          `${process.env.REACT_APP_BACKEND_URL}/new-restaurants`,
+          `${process.env.REACT_APP_BACKEND_URL}/contact-us-forms `,
           "POST",
           {
             "Content-Type": "application/json",
@@ -29,8 +28,10 @@ const ContactQureyTable = (props) => {
             userId,
           })
         );
+        console.log("#########################################");
         console.log("responseData", responseData);
-        setData(responseData.existingRestaurantsAdmin);
+        console.log("#########################################");
+        setData(responseData.contactForms);
       } catch (err) {
         // console.log("err", err);
       }
@@ -38,99 +39,19 @@ const ContactQureyTable = (props) => {
     if (token && userId) dashboard();
   }, [token, userId, sendRequest]);
 
-  const emailRef = useRef();
-  useEffect(() => {
-    const timer = setTimeout(async () => {
-      //   if (searchByEmail === emailRef.current.value) {
-      try {
-        const responseData = await sendRequest(
-          `${process.env.REACT_APP_BACKEND_URL}/get-restaurant-by-email`,
-          "POST",
-          {
-            "Content-Type": "application/json",
-            Authorization: "Bearer " + token,
-          },
-          JSON.stringify({
-            userId,
-            emailQuery: searchByEmail,
-            type: "New",
-          })
-        );
-        console.log("responseData", responseData);
-        setData(responseData.restaurants);
-      } catch (err) {}
-      //   }
-    }, 500);
-    return () => {
-      clearTimeout(timer);
-    };
-  }, [searchByEmail, sendRequest, userId]);
-
-  const nameRef = useRef();
-  useEffect(() => {
-    const timer = setTimeout(async () => {
-      //   if (searchByName === nameRef.current.value) {
-      try {
-        const responseData = await sendRequest(
-          `${process.env.REACT_APP_BACKEND_URL}/get-restaurant-by-name`,
-          "POST",
-          {
-            "Content-Type": "application/json",
-            Authorization: "Bearer " + token,
-          },
-          JSON.stringify({
-            userId,
-            emailQuery: searchByName,
-            type: "New",
-          })
-        );
-        console.log("responseData", responseData);
-        setData(responseData.restaurants);
-      } catch (err) {}
-      //   }
-    }, 500);
-    return () => {
-      clearTimeout(timer);
-    };
-  }, [searchByName, sendRequest, userId]);
-
   let content;
-  // if (!isLoading && data)
-  // content = data.map((item) => {
-  //   return (
-  //     <tr>
-  //       <td className="orderTableTD">{item.name}</td>
-  //       <td className="orderTableTD">{item.email}</td>
-  //       <td className="orderTableTD">{item.addres}</td>
-  //       <td className="orderTableTD">
-  //         {item.approved ? "Approved" : "Not Approved"}
-  //       </td>
-  //       <td className="orderTableTD">
-  //         <i
-  //           onClick={() => props.editRestaurant(item._id)}
-  //           style={{ cursor: "pointer" }}
-  //           class="far fa-edit"
-  //         ></i>
-  //       </td>
-  //     </tr>
-  //   );
-  // });
-  content = [...Array(5)].map((item, i) => {
-    return (
-      <tr>
-        <td className="orderTableTD">Faisal</td>
-        <td className="orderTableTD">admin@admin.com</td>
-        <td className="orderTableTD">+92-307-123-4567</td>
-        <td className="orderTableTD">I want to register with snaxa</td>
-        {/* <td className="orderTableTD">
-          <i style={{ cursor: "pointer" }} class="far fa-edit"></i>
-          <label className="reviewTableViewButton mr-2">View</label>
-          <label className="reviewTableDeleteButton">Delete</label>
-        </td> */}
-      </tr>
-    );
-  });
-  // else content = <p>Loading...</p>;
+  if (!isLoading && data)
+    content = data.map((item, i) => {
+      return (
+        <tr key={i}>
+          <td className="orderTableTD">{item.name}</td>
+          <td className="orderTableTD">{item.email}</td>
+          <td className="orderTableTD">{item.number}</td>
+          <td className="orderTableTD">{item.comment}</td>
+        </tr>
+      );
+    });
+  else content = <p>Loading...</p>;
   return (
     <div className="restaurantmanagementtable mb-4">
       <div class="container">
